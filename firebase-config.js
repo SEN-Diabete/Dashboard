59f056ba23aa073091e491fe94e7c19ff3092882
SEN-Diabete <sendiabete@gmail.com>
2025-07-06 22:59:10 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-// Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyC4T4cc-WcvBsts7dmmqh_I1cFLzuaxKos",
   authDomain: "sendiabete-app.firebaseapp.com",
   projectId: "sendiabete-app",
-  storageBucket: "sendiabete-app.firebasestorage.app",
+  storageBucket: "sendiabete-app.appspot.com",
   messagingSenderId: "1059445382855",
   appId: "1:1059445382855:web:101cdd6b28b5c76c9ae8e2"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
