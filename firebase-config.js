292d8abb6e91575fcab29618e873a0dc17ff0356
SEN-Diabete <sendiabete@gmail.com>
2025-07-06 22:55:14 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC4T4cc-WcvBsts7dmmqh_I1cFLzuaxKos",
   authDomain: "sendiabete-app.firebaseapp.com",
   projectId: "sendiabete-app",
