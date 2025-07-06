4108d4036c450d6234e74dbda6c298f9b92b668e
SEN-Diabete <sendiabete@gmail.com>
2025-07-06 22:35:19 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4T4cc-WcvBsts7dmmqh_I1cFLzuaxKos",
+  authDomain: "sendiabete.firebaseapp.com",
+  projectId: "sendiabete",
+  storageBucket: "sendiabete.appspot.com",
+  messagingSenderId: "1059445382855",
+  appId: "1:123456789:web:xxxxxx"
+};
